1993a77bdaace12125bde2fcbaecd044f8f8d436
Shibu Lekshman <shibu9389@yahoo.com>
2014-06-04 11:19:41 -0400

    Second commit
    Second commit

---
+++ customer/404.php
@@ -0,0 +1,20 @@
+<?php
+/**
+* @project ApPHP Hotel Site
+* @copyright (c) 2010 - 2013 ApPHP
+* @author ApPHP <info@apphp.com>
+* @license http://www.gnu.org/licenses/
+*/
+
+// *** Make sure the file isn't accessed directly
+defined('APPHP_EXEC') or die('Restricted Access');
+//--------------------------------------------------------------------------
+
+// draw title bar
+draw_title_bar(_CUSTOMERS); 
+
+draw_content_start();
+draw_important_message(_PAGE_NOT_EXISTS);
+draw_content_end();
+
+?>
